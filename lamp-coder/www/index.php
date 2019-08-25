83a7ab9e34d26c6da361cbb9bb0e052382f9864b
Steve Verhagen <scverhagen@gmail.com>
2019-08-25 17:55:55 -0500

    lamp-coder: add sample index.php file

---
+++ lamp-coder/www/index.php
@@ -0,0 +1,4 @@
+<?php ?>
+<h2>It works!</h2>
+<a href="http://<? echo $_SERVER['SERVER_NAME'];?>:8080" target="_blank">phpmyadmin</a><br>
+<a href="http://<? echo $_SERVER['SERVER_NAME'];?>:8443 target="_blank">Visual Studio Code</a>
